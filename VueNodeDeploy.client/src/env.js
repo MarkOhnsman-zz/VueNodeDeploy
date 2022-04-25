b85213e1da27a0e617b0a5cf9bf3f58e2e77ca2c
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2022-04-25 12:16:28 -0600

    adds env

--- VueNodeDeploy.client/src/env.js
+++ VueNodeDeploy.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-geue0lhy.us.auth0.com'
+export const audience = 'https://mark-dev.com'
+export const clientId = 'NM80zhquib2VaNIlqci7Zt6tgu7BwUTx'
